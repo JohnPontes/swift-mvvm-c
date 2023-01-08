136c85d5b7395489e92d961a8f7692c4862e612e
John Pontes <jwesleysilvap@gmaill.com>
2023-01-08 19:29:15 -0300

    Model, Manager, Business and Provider classes

---
+++ SwiftArquiteturas/Infrastructure/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SwiftArquiteturas
+//
+//  Created by John Pontes on 08/01/23.
+//
+
+import Foundation
+
+struct Constants {
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "userModel"
+    }
+}
